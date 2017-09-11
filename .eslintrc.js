10c78e944d56d1b94163f789538f1aa214fa7252
qetuozm <yinjianihao@qq.com>
2017-09-11 17:57:05 +0800

    eslint提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'indent': ['error', 4],
-    'space-before-function-paren': ['error', 'never'],
+	'eol-last': 0,
+    'space-before-function-paren': ['error', 'never']
+	
   }
 }
