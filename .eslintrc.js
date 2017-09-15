2b48d14ba292e3ab609c000ef37d09b29ea80a1a
qetuozm <yinjianihao@qq.com>
2017-09-15 15:04:37 +0800

    登录接口

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'indent': ['error', 4],
 	'eol-last': 0,
-    'space-before-function-paren': ['error', 'never']
-	
+	"space-before-function-paren": ["error", "never"]
   }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express')
+let router = express.Router()
 
+let User = require('../models/users')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+    res.send('respond with a resource')
+})
 
-module.exports = router;
+router.post('/login', function(req, res, next) {
+    let param = {
+        userName: req.body.userName,
+        userPwd: req.body.userPwd
+    }
+    User.findOne(param, (err, userDoc) => {
+        if (err) {
+            res.json({
+                status: '1',
+                msg: err.message
+            })
+        } else {
+            if (userDoc) {
+                res.cookie('userId', userDoc.userId, {
+                    path: '/',
+                    maxAge: 1000 * 60 * 60
+                })
+                // req.session.user = userDoc
+                res.json({
+                    status: '0',
+                    msg: '',
+                    result: {
+                        userName: userDoc.userName
+                    }
+                })
+            } else {
+                res.json({
+                    status: '1',
+                    msg: '用户名或者密码错误'
+                })
+            }
+        }
+    })
+})
+
+module.exports = router
